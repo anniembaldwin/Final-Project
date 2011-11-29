0478aaa5d2f3d9b33e84babbcad1a6d13231dcd2
njacewicz <njacewicz@college.harvard.edu>
2011-11-29 11:21:21 -0500

    images uploaded

---
+++ items.js
@@ -0,0 +1,322 @@
+/****************************************************************************
+ * items.js
+ *
+ * Computer Science 50
+ * Recycle-Harvard
+ *
+ * Structs for items.
+ ***************************************************************************/
+
+var ITEMS = [
+ {
+  name: "ballpoint pen",
+  status: "trash"
+ },
+ {
+  name: "banana",
+  status: "trash"
+ },
+ {
+  name: "bubblewrap",
+  status: "trash"
+ },
+ {
+  name: "candy wrapper",
+  status: "trash"
+ },
+ {
+  name: "ceramic mug",
+  status: "trash"
+ },
+ {
+  name: "china plate",
+  status: "trash"
+ },
+ {
+  name: "bag of chips",
+  status: "trash"
+ },
+ {
+  name: "crepe paper",
+  status: "trash"
+ },
+ {
+  name: "drinking glass",
+  status: "trash"
+ },
+ {
+  name: "dryer sheet",
+  status: "trash"
+ },
+ {
+  name: "laboratory glass",
+  status: "trash"
+ },
+ {
+  name: "laminated paper",
+  status: "trash"
+ },
+ {
+  name: "metal box",
+  status: "trash"
+ },
+ {
+  name: "mirror",
+  status: "trash"
+ },
+ {
+  name: "napkin",
+  status: "trash"
+ },
+ {
+  name: "paper towel",
+  status: "trash"
+ },
+ {
+  name: "plastic bag",
+  status: "trash"
+ },
+ {
+  name: "plastic fork",
+  status: "trash"
+ },
+ {
+  name: "plastic spoon",
+  status: "trash"
+ },
+ {
+  name: "plastic wrap",
+  status: "trash"
+ },
+ {
+  name: "Ramen noodles cup",
+  status: "trash"
+ },
+ {
+  name: "rubber bands",
+  status: "trash"
+ },
+ {
+  name: "styrofoam box",
+  status: "trash"
+ },
+ {
+  name: "styrofoam cup",
+  status: "trash"
+ },
+ {
+  name: "tempered glass",
+  status: "trash"
+ },
+ {
+  name: "tissues",
+  status: "trash"
+ },  {
+  name: "vhs video tape",
+  status: "trash"
+ },
+ {
+  name: "wax paper",
+  status: "trash"
+ },
+ {
+  name: "wooden box",
+  status: "trash"
+ },
+ {
+  name: "ziplock bag",
+  status: "trash"
+ },
+ {
+  name: "aluminum can",
+  status: "recycle"
+ },
+ {
+  name: "box from Amazon",
+  status: "recycle"
+ },
+ {
+  name: "beer bottle",
+  status: "reycle"
+ },  
+ {
+  name: "bottle cap",
+  status: "recycle"
+ },
+ {
+  name: "paper bag",
+  status: "recycle"
+ },
+ {
+  name: "cardboard takeout box",
+  status: "recycle"
+ },
+ {
+  name: "detergent bottle",
+  status: "reycycle"
+ },
+ {
+  name: "foil takeout box",
+  status: "recycle"
+ },
+ {
+  name: "Frappuccino cup",
+  status: "recycle"
+ },
+ {
+  name: "glass jar",
+  status: "recycle"
+ },
+  {
+  name: "hardback book",
+  status: "recycle"
+ },
+ {
+  name: "Harvard Crimson",
+  status: "recycle"
+ },
+ {
+  name: "ice cream carton",
+  status: "recycle"
+ },
+ {
+  name: "juice box",
+  status: "reycycle"
+ },
+ {
+  name: "junk mail",
+  status: "recycle"
+ },
+ {
+  name: "bottle of soda",
+  status: "recycle"
+ },
+ {
+  name: "magazines",
+  status: "recycle"
+ },
+  {
+  name: "paper milk carton",
+  status: "recycle"
+ },
+ {
+  name: "notebook paper",
+  status: "recycle"
+ },
+ {
+  name: "paper cup",
+  status: "recycle"
+ },
+ {
+  name: "paper plate",
+  status: "reycycle"
+ },
+ {
+  name: "paper shopping bag",
+  status: "recycle"
+ },
+ {
+  name: "paperback book",
+  status: "recycle"
+ },
+ {
+  name: "pizza box",
+  status: "recycle"
+ },
+   {
+  name: "plastic milk carton",
+  status: "recycle"
+ },
+ {
+  name: "postcard",
+  status: "recycle"
+ },
+ {
+  name: "receipt",
+  status: "recycle"
+ },
+ {
+  name: "envelope",
+  status: "reycycle"
+ },
+ {
+  name: "shredded paper",
+  status: "recycle"
+ },
+ {
+  name: "solo cup",
+  status: "recycle"
+ },
+ {
+  name: "stamp",
+  status: "recycle"
+ },
+   {
+  name: "Starbucks cup",
+  status: "recycle"
+ },
+ {
+  name: "Starbucks frappuccino bottle",
+  status: "recycle"
+ },
+ {
+  name: "tin foil",
+  status: "recycle"
+ },
+ {
+  name: "tupperware",
+  status: "reycycle"
+ },
+ {
+  name: "plastic water bottle",
+  status: "recycle"
+ },
+ {
+  name: "box from Zappos",
+  status: "recycle"
+ },
+ {
+  name: "laptop",
+  status: "e-waste"
+ },
+ {
+   name: "batteries",
+  status: "e-waste"
+ },
+  {
+  name: "cell phone battery",
+  status: "e-waste"
+ },
+ {
+   name: "cell phone",
+  status: "e-waste"
+ },
+  {
+  name: "CFL",
+  status: "e-waste"
+ },
+ {
+   name: "computer tower",
+  status: "e-waste"
+ },
+  {
+  name: "computer monitor",
+  status: "e-waste"
+ },
+ {
+   name: "DVD player",
+  status: "e-waste"
+ },
+  {
+  name: "ink cartridge",
+  status: "e-waste"
+ },
+ {
+   name: "printer",
+  status: "e-waste"
+ },
+  {
+  name: "television",
+  status: "e-waste"
+ },
+]
