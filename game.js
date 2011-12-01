08d6d205677af5b005773fc9b172c3e18552ac87
Annie Baldwin <abaldwin@college.harvard.edu>
2011-12-01 00:45:12 -0500

    second game

---
+++ game.js
@@ -0,0 +1,14 @@
+/****************************************************************************
+ * game.js
+ *
+ * Computer Science 50
+ * Problem Set 8
+ *
+ * Interface for recycling game
+ ***************************************************************************/
+ 
+ <html>
+<script type="text/javascript">
+document.getElementById("demo").innerHTML=Date();
+</script>
+ 
