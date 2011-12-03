4ab98db2c0ad65bf1c1e6807d16136e7ed9ba851
Annie Baldwin <abaldwin@college.harvard.edu>
2011-12-03 04:48:30 -0500

    register.php

--- register2.php
+++ register2.php
@@ -12,20 +12,20 @@
     // require common code
     require_once("includes/common.php"); 
 
-    // escape username and passwords to avoid SQL injection attacks
+    // escape username and house to avoid SQL injection attacks
     $username = mysql_real_escape_string($_POST["username"]);
     $house = mysql_real_escape_string($_POST["house"]);
     
     // make sure that $_POST["username"] or $_POST["password"] is blank, return to apology page
     if (empty($username)  || empty($_POST["password"]) || empty($house))
         apologize("please fill in all required fields"); 
-   
-    // define a hash of the password
-    $hash = crypt($_POST["password"]); 
     
-   // make sure that both passwords are the same
+    // make sure that both passwords are the same
     if ($_POST["password"] != $_POST["password2"])
         apologize("please make sure both passwords are the same"); 
+    
+    // define a hash of the password
+    $hash = crypt($_POST["password"]); 
     
     // prepare SQL
     $sql = "INSERT INTO users (username, hash, house) VALUES ('$username', '$hash', '$house')";
@@ -34,7 +34,7 @@
     $result = mysql_query($sql);
 
     // make sure that username is actually unique
-    if (mysql_num_rows($result)== 1)
+    if ($result == null)
         apologize("that username is already taken"); 
             
     // find out which id was assigned to that user
