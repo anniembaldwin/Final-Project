8778d19dc64f17b2660ce083223c82c8227653d8
Annie Baldwin <abaldwin@college.harvard.edu>
2011-12-07 16:53:34 -0500

    data return type

--- game.php
+++ game.php
@@ -61,7 +61,7 @@
             
             // send the status and receptacle data to game2.php for validation and points update    
             $.get("game2.php",{status:status, bin:bin},function(data){
-                $("#Correctness").html(data);
+                $("#Correctness").html(data), "text";
                       
             // load a new image to the page for them to evaluate     
             random_image();
