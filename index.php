734d41fba1b6673dcae08382e13436b1ebf78cc2
Annie Baldwin <abaldwin@college.harvard.edu>
2011-12-07 11:57:30 -0500

    css

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 
 <html>
   <head>
-    <link href="/css/styles.css" rel="stylesheet" type="text/css" />
+    <link href="css/styles.css" rel="stylesheet" type="text/css" />
     <title>RecycleHarvard: Welcome!</title>
   </head>
   
