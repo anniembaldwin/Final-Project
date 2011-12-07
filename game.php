86e5ccad1cdfc78af756e46ac670f77e85250035
Annie Baldwin <abaldwin@college.harvard.edu>
2011-12-07 17:57:20 -0500

    json

--- game.php
+++ game.php
@@ -54,8 +54,8 @@
        
        // after the page is loaded and the trash is clicked, check whether that item belongs in the trash
        $(document).ready(function(){  
-            $("#trash").click(function(e) {
-            console.log(e);
+            $("#trash").click(function() {
+            
             // remember what bin was clicked (trash)  
             var bin = $("img.trash").attr('alt');
             
