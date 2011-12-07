ac874414c690d424cda9202683c2c519791a016b
Annie Baldwin <abaldwin@college.harvard.edu>
2011-12-07 18:23:09 -0500

    json fix3

--- game.php
+++ game.php
@@ -62,7 +62,7 @@
             // send the status and receptacle data to game2.php for validation and points update    
             $.get("game2.php",{status:status, bin:bin},function(data){
                 $("#Correctness").html(data.correct);
-                //$("#points").html(data.points);
+                $("#points").html(data.points);
                       
             // load a new image to the page for them to evaluate     
             random_image();
