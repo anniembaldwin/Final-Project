ffd0add0bde81420c1ae116e60b1f7138e7194a4
Annie Baldwin <abaldwin@college.harvard.edu>
2011-12-08 09:59:03 -0500

    return to

--- logout.php
+++ logout.php
@@ -1,4 +1,4 @@
-<?
+ <?
 /****************************************************************************
  *                               logout.php
  *
@@ -8,7 +8,29 @@
  *
  *                            Logs out a user.
  ***************************************************************************/
+
+    // require common code
+    require_once("includes/common.php"); 
+ 
+    // configuration
+    //require_once(dirname(__FILE__) . "/config.php");
+ 
+    // log out current user, forgetting their session id
+    logout();
+ 
+    /* if user is already logged in, log out
+    if (isset($_SESSION["user"]))
+        unset($_SESSION["user"]);*/
+ 
+ 
+    // redirect user to index.php
+    $protocol = (isset($_SERVER["HTTPS"])) ? "https" : "http";
+    $host  = $_SERVER["HTTP_HOST"];
+    $path = rtrim(dirname($_SERVER["PHP_SELF"]), "/\\");
+    //header("Location: {$protocol}://{$host}{$path}/logout.php");
+ 
  ?>
+
 <!DOCTYPE html>
 
 <html>
