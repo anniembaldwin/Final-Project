0d3534c7fb9374b8f14b4727970c3ede782e58ed
Annie Baldwin <abaldwin@college.harvard.edu>
2011-12-07 21:32:29 -0500

    loginphp

--- config.php
+++ config.php
@@ -10,7 +10,7 @@
  
     // URL that CS50 ID should ask users to trust; must be registered with CS50,
     // per the HOWTO at https://manual.cs50.net/ID; must be a prefix of RETURN_TO
-    define("TRUST_ROOT", "http://cloud.cs50.net/~abaldwin");
+    define("TRUST_ROOT", "http://cloud.cs50.net/~abaldwin/");
  
     // URL to which CS50 ID should return users; must be registered with CS50,
     // per the HOWTO at https://manual.cs50.net/ID
