de87ace1c89c24aad3d8a35c3818bc7843482fed
Annie Baldwin <abaldwin@college.harvard.edu>
2011-12-07 23:21:47 -0500

    jsonfinal debug final final

--- game.php
+++ game.php
@@ -92,7 +92,7 @@
                       
             // load a new image to the page for them to evaluate     
             random_image();  
-            }}),
+            }});
                  
       });
      
