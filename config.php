ea1f39f38ae4d8c893d6c5e39e0cf0d514514cfd
Annie Baldwin <abaldwin@college.harvard.edu>
2011-12-07 23:40:33 -0500

    new version https

--- config.php
+++ config.php
@@ -14,7 +14,7 @@
  
     // URL to which CS50 ID should return users; must be registered with CS50,
     // per the HOWTO at https://manual.cs50.net/ID
-    define("RETURN_TO", "https://cloud.cs50.net/abaldwin/return_to.php");
+    define("RETURN_TO", "https://cloud.cs50.net/~abaldwin/return_to.php");
  
     // CS50 Library; ideally, this should not be inside public_html (or DocumentRoot)
     require_once("CS50/CS50.php");
